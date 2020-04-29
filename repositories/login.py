ef142f43e62e7a21e2969561811b1c9bf9c39aed
Daniel Coady <daniel.coady@outlook.com.au>
2020-04-29 19:30:08 +1000

    added user authentication

--- repositories/login.py
+++ repositories/login.py
@@ -1,15 +1,27 @@
 from flask import request, Response
 from flask_restful import Resource
+import json
+import database as db
 
 class Login(Resource):
     # logs the user in
     # for now we will just accept usernames. if we want to go bigger then
     # i'll start considering passwords
     def post(self):
-        username = request.json["username"]
-        # TODO: check if user exists in db, if they do log in if not reject login
-        # we'll just pretend that only i can get in for now
-        if username == "pondo":
-            return Response("{ 'message' : 'User authenticated' }", status=200, mimetype="application/json")
-        else:
-            return Response("{ 'message' : 'Failed to authenticate user' }", status=401, mimetype="application/json")
+        try:
+            username = request.json["username"]
+            query = "SELECT * FROM users WHERE username = '" + username + "'"
+            res = db.Execute(query)
+            if len(res) > 0:
+                response = {
+                    "message": "User authenticated",
+                    "userData": {
+                        "userId": res[0][0],
+                        "username": res[0][1]
+                    }
+                }
+                return Response(json.dumps(response), status=200, mimetype="application/json")
+            else:
+                return Response("{ 'message' : 'Failed to authenticate user' }", status=401, mimetype="application/json")
+        except:
+            return Response(status=500)
